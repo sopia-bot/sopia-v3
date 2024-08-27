e8ec3d3d2e73dd9c6f5abecc40015906ae49cd35
raravel <youn@tree-some.dev>
2024-08-28 00:53:48 +0900

    현재곡 멜론플레이어 캡션 가져기 개선

--- official-bundles/now-song/index.js
+++ official-bundles/now-song/index.js
@@ -1,23 +1,41 @@
 const path = window.require('path');
-
+const { execSync } = window.require('child_process');
+const iconv = window.require('iconv-lite');
 const { getNowSong } = window.require(path.join(__dirname, './song'));
 
 let lastSong = '';
 
-window.getNowSongInfo = (sock) => {
-	const caption = getNowSong().trim();
-	if ( caption === '' ) {
-		return;
-	}
 
-	if ( caption.includes('ActiveMovie Window') ) {
-		sock.message('멜론 플레이어를 한 번 클릭해 주세요.');
-		return;
-	}
-
-	const delMelonCaption = caption.replace(/ - melon$/i, '').trim();
+function getMelonCaptionExec() {
 	try {
-		let [ song, title, singer ] = delMelonCaption.match(/(.*?) - (.*)/);
+		const buf = execSync('powershell.exe -Command "Get-Process \\"Melon Player\\" | Format-List -Property MainWindowTitle "');
+		const line = iconv.decode(buf, 'euc-kr');
+		const caption = line.split('\n').find((s) => s.replace('MainWindowTitle : ', '').trim())
+			.replace('MainWindowTitle : ', '').trim();
+		return caption;
+	} catch (e) {
+		return e.message;
+	}
+}
+
+window.getNowSongInfo = (sock) => {
+	let caption = getMelonCaptionExec();
+
+	if ( !caption ) {
+		const caption = getNowSong().trim();
+		if ( caption === '' ) {
+			return;
+		}
+
+		if ( caption.includes('ActiveMovie Window') ) {
+			sock.message('멜론 플레이어를 한 번 클릭해 주세요.');
+			return;
+		}
+
+		caption = caption.replace(/ - melon$/i, '').trim();
+	}
+	try {
+		let [ song, title, singer ] = caption.match(/(.*?) - (.*)/);
 		return { title, singer };
 	} catch(err) {
 		console.error(err);
